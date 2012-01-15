79fcd4332015d33550f86e49249611d21196b104
Andy Freeland <andy@andyfreeland.net>
2012-01-15 17:25:58 -0600

    added logdir variable

--- src/logger/start-logger.sh
+++ src/logger/start-logger.sh
@@ -8,5 +8,5 @@ source ./logging-config
 # start daemonlogger
 for interface in "eth0" "eth1" "eth2"
 do
-    sudo /usr/bin/daemonlogger -l /home/logger/logs -i $interface -n $interface.pcap -t $ROLLOVER_TIME -u logger -d
+    sudo /usr/bin/daemonlogger -l $LOGDIR -i $interface -n $interface.pcap -t $ROLLOVER_TIME -u logger -d
 done
