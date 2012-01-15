27b13f68e6069b9710c6b0aa1a01e221a1b186f3
Andy Freeland <andy@andyfreeland.net>
2012-01-15 16:12:33 -0600

    router-logger.sh logs to ~/logs

--- src/logger/router-logger.sh
+++ src/logger/router-logger.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo /usr/bin/daemonlogger -l /home/logger -i eth0 -n eth0.pcap -t 5m -u logger -d
-sudo /usr/bin/daemonlogger -l /home/logger -i eth1 -n eth1.pcap -t 5m -u logger -d
-sudo /usr/bin/daemonlogger -l /home/logger -i eth2 -n eth2.pcap -t 5m -u logger -d
+sudo /usr/bin/daemonlogger -l /home/logger/logs -i eth0 -n eth0.pcap -t 5m -u logger -d
+sudo /usr/bin/daemonlogger -l /home/logger/logs -i eth1 -n eth1.pcap -t 5m -u logger -d
+sudo /usr/bin/daemonlogger -l /home/logger/logs -i eth2 -n eth2.pcap -t 5m -u logger -d
