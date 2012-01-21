2e7fb118ad3cc461218f3bab127f3cfc766fa728
Andy Freeland <andy@andyfreeland.net>
2012-01-21 15:18:29 -0600

    added some initial fields for the packet model

--- src/honeynet_web/honeywall/models.py
+++ src/honeynet_web/honeywall/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Packet(models.Model):
+    source_ip = models.IntegerField()
+    destination_ip = models.IntegerField()
+    # protocal can be 0-255
+    protocal = models.IntegerField()
+
+    # should attacks be a many-to-one relationship or many-to-many?
+    # i.e. can a single packet be used in multiple attacks?
+    attack = models.ForeignKey('Attack')
+    # other fields:
+    ## data
+    ## length
+    ## identification
+    ## time
+    ## options
+    ## flags
+    ## ???
+
+class Attack(models.Model):
+    # fields:
+    ## packets
+    ## classification
+    ## source
+    ## target
+    ## time
+    ## ???
+    pass
