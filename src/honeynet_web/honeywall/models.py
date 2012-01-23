932114638131671921c0ad01a139e7bc27c5c506
Andy Freeland <andy@andyfreeland.net>
2012-01-22 19:01:09 -0600

    updated models

--- src/honeynet_web/honeywall/models.py
+++ src/honeynet_web/honeywall/models.py
@@ -1,29 +1,23 @@
 from django.db import models
 
+class Attack(models.Model):
+    classification_time = models.DateField(auto_now_add=True)
+    source_ip = models.IPAddressField()
+    destination_ip = models.IPAddressField(null=True, blank=True)
+    start_time = models.DateField()
+    end_time = models.DateField(null=True, blank=True)
+    score = models.DecimalField(max_digits=6, decimal_places=3)
+
 class Packet(models.Model):
-    source_ip = models.IntegerField()
-    destination_ip = models.IntegerField()
+    source_ip = models.IPAddressField()
+    destination_ip = models.IPAddressField(null=True, blank=True)
+    source_port = models.IntegerField(null=True, blank=True)
+    dest_port = models.IntegerField(null=True, blank=True)
+    time = models.DateField()
+
     # protocal can be 0-255
     protocal = models.IntegerField()
+    payload = models.CharField(max_length=65535)
 
-    # should attacks be a many-to-one relationship or many-to-many?
-    # i.e. can a single packet be used in multiple attacks?
-    attack = models.ForeignKey('Attack')
-    # other fields:
-    ## data
-    ## length
-    ## identification
-    ## time
-    ## options
-    ## flags
-    ## ???
-
-class Attack(models.Model):
-    # fields:
-    ## packets
-    ## classification
-    ## source
-    ## target
-    ## time
-    ## ???
-    pass
+    attack = models.ForeignKey(Attack)
+    classification_time = models.DateField(auto_now_add=False, blank=True, null=True)
