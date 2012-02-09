62f75a63b61ea62e98f1feacefb27e8cadd40f92
Tsusa Matsui <matsuit@carleton.edu>
2012-02-09 17:25:50 -0600

    started mail threat analyzer

--- src/honeynet_web/packetAnalysis/analyzers/mail.py
+++ src/honeynet_web/packetAnalysis/analyzers/mail.py
@@ -23,4 +23,14 @@ class MailAnalyzer(AttackAnalyzer):
     type = 'mail'
     
     def addAttackProfile(self):
+        safeNodeIndex = self.addPrelimNode(300000) #5 minute timeout, for mail 
+        threatNodeIndex = self.addThreatNode(300000) #5 minute timeout, for mail
+        
+        self.addTransition(self.startNode, self.nodes[safeNodeIndex], 0, #condition) #started receiving mail
+            #condition = SMTP mail FROM, keep track of how many times this comes up w/ same ip
+        self.addTransition(self.nodes[safeNodeIndex], self.nodes[safeNodeIndex], 0, #condition)
+            #condition = SMTP rcpt TO or SMTP data or SMTP QUIT or anything else from same ip, SMTP
+        self.addTransition(self.nodes[safeNodeIndex], self.startNode, 0, #condition) #QUIT command received, moving back to start
+        self.addTransition(self.nodes[safeNodeIndex], self.nodes[threatNodeIndex], #some score, #condition) #receiving a lot of mail in one connection
+            #condition = SMTP mail FROM AND counter in self.nodes[safeNodeIndex] is huge
         pass
