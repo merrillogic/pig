ea8e51b78aba2d051365935ec15f1820796e0b87
garnaasj <garnaasj@pnueli.mathcs.carleton.edu>
2012-02-11 14:17:51 -0600

    triggers changed to list of functions

--- src/honeynet_web/packetAnalysis/analyzers/sqlinj.py
+++ src/honeynet_web/packetAnalysis/analyzers/sqlinj.py
@@ -37,9 +37,9 @@ class SQLInjectionAnalyzer(AttackAnalyzer):
         self.addTransition(0, 1, 0, [isQuery])
         for prelimIndex in range(1, numPrelims):
             #for the first numPrelims-1 nodes...
-            self.addTransition(prelimIndex, prelimIndex + 1, prelimIndex+1, isSameQuery)
+            self.addTransition(prelimIndex, prelimIndex + 1, prelimIndex+1, [isSameQuery])
             
         self.addThreatNode()
-        self.addTransition(-2, -1, numPrelims, isSameQuery)
-        self.addTransition(-1, -1, numPrelims, isSameQuery)
+        self.addTransition(-2, -1, numPrelims, [isSameQuery])
+        self.addTransition(-1, -1, numPrelims, [isSameQuery])
         
