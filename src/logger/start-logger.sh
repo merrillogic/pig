38df078db830014ee2b2e0fa00497f1ad28ac9bc
Andy Freeland <andy@andyfreeland.net>
2012-02-12 17:35:03 -0600

    Logger now rollsover at 2M or 30seconds, whichever comes first.

--- src/logger/start-logger.sh
+++ src/logger/start-logger.sh
@@ -8,5 +8,5 @@ source ./logging-config
 # start daemonlogger
 for interface in "br0" "eth1" "eth2"
 do
-    sudo /usr/bin/daemonlogger -l $LOGDIR -i $interface -n $interface.pcap -t $ROLLOVER_TIME -u logger -d
+    sudo /usr/bin/daemonlogger -l $LOGDIR -i $interface -n $interface.pcap -s $ROLLOVER_SIZE -t $ROLLOVER_TIME -u logger -d
 done
