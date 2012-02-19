d6a1b6edbb3bb56c4b7748a3a388a9b253dd5e9f
Chris Becerra <helpre@gmail.com>
2012-02-19 15:13:07 -0600

    parse_dir for directory parsing

---
+++ src/honeynet_web/honeywall/management/commands/parse_dir.py
@@ -0,0 +1,42 @@
+from cProfile import Profile
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from honeynet_web.honeywall.models import Packet
+from Packeteer import PacketReader
+import os
+
+class Command(BaseCommand):
+    args = '<pcap-dir>'
+    option_list = BaseCommand.option_list + (
+    make_option('--profile',
+        action='store_true',
+        dest='profile',
+        default=False,
+        help='profiles the command'),
+    )
+
+    help = 'Parses a directory of pcap files into the DB.'
+
+    @transaction.commit_manually
+    def _handle(self, *args, **options):
+        pcap_dir = args[0]
+        pcaps = os.listdir(pcap_dir)
+        for pcap_file in pcaps:
+            print "current file: ", pcap_dir+ pcap_file
+            pcap = PacketReader(pcap_dir+pcap_file)
+
+            for packet in pcap:
+                p = Packet(**packet)
+                p.save()
+
+        transaction.commit()
+
+    def handle(self, *args, **options):
+        if options['profile']:
+            profiler = Profile()
+            profiler.runcall(self._handle, *args, **options)
+            profiler.print_stats()
+        else:
+            self._handle(*args, **options)
+
