13e4b31282599c6a396ddd573ddea0949530f111
Andy Freeland <andy@andyfreeland.net>
2012-02-19 21:02:44 -0600

    attacks have unique constraint

--- src/honeynet_web/honeywall/models.py
+++ src/honeynet_web/honeywall/models.py
@@ -34,7 +34,7 @@ class Attack(models.Model):
     class Meta:
         # order newest first -- but by what measure?
         #ordering = ['-time']
-        pass
+        unique_together = ('time', 'source_ip', 'attack_type')
 
     def __str__(self):
         out = "Attack type: "+str(self.attack_type)+'\n'+\
