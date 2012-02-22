8a1cb48c06303d3b6b9dcda59182a7416161af42
garnaasj <garnaasj@yellow.mathcs.carleton.edu>
2012-02-22 15:54:51 -0600

    True not False to kill connections

--- src/honeynet_web/packetAnalysis/connection.py
+++ src/honeynet_web/packetAnalysis/connection.py
@@ -37,7 +37,7 @@ class AttackProcess(object):
         self.lock.release()
         
     def killConnection(self):
-        self.pipe.send(False)
+        self.pipe.send(True)
         self.process.join()
     
     def getMessage(self):
