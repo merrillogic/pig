c0827301c4238543b6f78e6ff7c0011c82962198
Chris Becerra <helpre@gmail.com>
2012-02-22 06:11:01 -0600

    Tua laetitia mea est, Andy. DoS now looks for packet rate of 200 per sec instead of 50

--- src/honeynet_web/packetAnalysis/analyzers/dos.py
+++ src/honeynet_web/packetAnalysis/analyzers/dos.py
@@ -23,10 +23,10 @@ class DOSAnalyzer(AttackAnalyzer):
     attackType = 'dos'
 
     def addAttackProfile(self):
-        for i in range(50):
+        for i in range(190):
             funct = lambda x: True
-            self.addPrelimNode(21)
+            self.addPrelimNode(5.1)
             self.addTransition(i, i+1, 1, [funct])
-        threat = self.addThreatNode(21)
+        threat = self.addThreatNode(5.1)
         self.addTransition(i+1, threat, 1, [funct])
         self.addTransition(threat, threat, 1, [funct])
