2e0f1b73bdfbd449158e65082a380a13e107aed8
Andy Freeland <andy@andyfreeland.net>
2012-02-22 23:20:36 -0600

    oops, we want it to copy when it's not already copying, not when it is already copying

--- src/logger/copy-logs.sh
+++ src/logger/copy-logs.sh
@@ -6,7 +6,7 @@ cd "`dirname "$0"`"
 source ./logging-config
 
 cd $LOGDIR
-if [ -n "$(ps aux | grep parse_pcap | grep -v grep)" ];
+if [ -z "$(ps aux | grep parse_pcap | grep -v grep)" ];
 then
     find . -type f -mmin +1 -print | xargs -n 1 $HOME/honeynet/src/honeynet_web/manage.py parse_pcap --settings=honeynet_web.settings_production
 fi
