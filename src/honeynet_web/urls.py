77c788eac9f2aae85220a45350928a7c53b89c89
Andy Freeland <andy@andyfreeland.net>
2012-02-25 17:28:12 -0600

    turned off django admins since we do not use it anyways

--- src/honeynet_web/urls.py
+++ src/honeynet_web/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from tastypie.api import Api
 from honeywall.api import AttackResource, PacketResource, ClassifyResource
 
-admin.autodiscover()
+#admin.autodiscover()
 
 v1_api = Api(api_name='v1')
 v1_api.register(AttackResource())
